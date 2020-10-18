f15b3ac3ca70039d1d4e9b17827b74f7455e2fd0
varunn10 <39978783+varunn10@users.noreply.github.com>
2020-10-18 18:39:57 +0530

    Add files via upload

---
+++ string right shift.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+	int i,x,j,l;
+	printf("ENTER SIZE OF ARRAY : ");
+	scanf("%d",&l);
+	char a[l],a_last;
+	printf("ENTER A STRING : ");
+	scanf("%s",a);
+	printf("ENTER ROTATION FACTOR : ");
+	scanf("%d",&x);
+	for(j=0;j<x;j++)
+	{
+		a_last = a[l-1];
+		for(i=l-1;i>=0;i--)
+		{
+			if(i>0)
+			{
+				a[i]=a[i-1];
+			}
+			else
+			{
+				a[i] = a_last;
+			}
+		}
+	}
+	printf("%s",a);
+}
